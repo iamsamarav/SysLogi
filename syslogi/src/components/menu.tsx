557fc4d965c608320f66608b7d54b233717a0ec8
Samara Vitória <devsamaravitoria@gmail.com>
2023-11-12 21:25:51 -0300

    Menu toggle OK

--- syslogi/src/components/menu.tsx
+++ syslogi/src/components/menu.tsx
@@ -3,63 +3,45 @@ import { AiFillAppstore } from "react-icons/ai";
 import Link from 'next/link';
 import { useSpring, animated } from "@react-spring/web";
 
-export default function Menu(){
-    const [menuOpen, setMenuOpen] = useState(false);
+export default function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-        if(menuOpen){
-            api.start({
-                from: {
-                  x: 100,
-                },
-                to: {
-                  x: 0,
-                },
-              })
-        }else{
-            api.start({
-                from: {
-                  x: 0,
-                },
-                to: {
-                  x: 100,
-                },
-              })
-        }
-    };
+  const fade = useSpring({
+    opacity: menuOpen ? 1 : 0,
+    y: menuOpen ? 0 : -20,
+    onRest: () => {
+      // Executa quando a animação termina
+      if (!menuOpen) {
+        setMenuClose(false);
+      }
+    },
+  });
 
-    const [animation, api] = useSpring(() =>{
-        if(menuOpen){
-            from: {x: 100}
-        }else{
-            from: {x: 0}
-        }
-      });
+  const [menuClose, setMenuClose] = useState(false);
 
-    return(
-        <div onClick={toggleMenu} className="w-1/3 flex items-center justify-center h-full text-xl flex-col">
-            <AiFillAppstore className="h-20 w-7 absolute"/>
-            {menuOpen && (
-        <animated.div className="relative -bottom-28 bg-cyan-300 rounded-l-2xl p-5 text-black box-border w-52 ...animation" style={animation}>
-          <ul>
-            <li className="border-black border p-2 rounded-l-full m-1">
-              <Link href="/">Home
-              </Link>
-            </li>
-            <li className="border-black border p-2 rounded-l-full m-1">
-              <Link href="/sobre">
-                Sobre
-              </Link>
-            </li>
-            <li className="border-black border p-2 rounded-l-full m-1">
-              <Link href="/contato">
-                Contato
-              </Link>
-            </li>
-          </ul>
-        </animated.div>
-      )}
-        </div>
-    )
+  const toggleTransform = () => {
+    setMenuOpen(!menuOpen);
+    if (!menuOpen) {
+      setMenuClose(true);
+    }
+  };
+
+  return (
+    <div className="w-1/3 flex items-center justify-center h-full text-xl flex-col">
+      <AiFillAppstore onClick={toggleTransform} className="h-20 w-7 absolute" />
+      <animated.div className={menuClose ? "relative -bottom-28 rounded-l-2xl text-black box-border w-52 bg-" : 'hidden'} style={fade}>
+        <ul>
+          <li className="border-black border p-2 rounded-l-full m-1 bg-blue-500">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="border-black border p-2 rounded-l-full m-1 bg-blue-500">
+            <Link href="/sobre">Sobre</Link>
+          </li>
+          <li className="border-black border p-2 rounded-l-full m-1 bg-blue-500">
+            <Link href="/contato">Contato</Link>
+          </li>
+        </ul>
+      </animated.div>
+    </div>
+  );
 }
