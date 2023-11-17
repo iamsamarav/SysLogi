46d3d510645f7267e300144547588cad4080912d
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-11-17 16:48:08 -0300

    Login Screen Init

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='w-full h-full flex flex-rows justify-center'>
+      <div className='bg-black text-white'>
+        <div className=''>
+
+        </div>
+      </div>
+
+      
+      <div className='bg-white text-black'>
+      </div> 
+    </div>
+  )
+}
